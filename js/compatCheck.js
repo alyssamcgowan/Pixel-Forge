b5ac19786d5517ba78daeb9ed033c1bb92cbb275
alyssa mcgowan <68068692+alyssamcgowan@users.noreply.github.com>
2024-07-23 21:59:01 -0400

    mobile popup fix attempt

--- js/compatCheck.js
+++ js/compatCheck.js
@@ -1,8 +1,15 @@
+var mobilepopup = document.querySelector('#mobilePopup');
+var mobilepopupbg = document.querySelector('#mobilePopupBG');
+
 window.addEventListener("load", (event) => {
     if(detectMobile()){
-        
+        mobilepopup.display = "block";
+        mobilepopupBG.display = "block";
+        console.log("mobile");
+    }else{
+        console.log("not mobile");
     }
-  
+    
   
 });
   
