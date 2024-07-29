d5bc6891344bb709b73df2afff70fd49652fc7a2
alyssa mcgowan <68068692+alyssamcgowan@users.noreply.github.com>
2024-07-28 20:08:38 -0400

    more popups

--- js/popupManager.js
+++ js/popupManager.js
@@ -8,11 +8,28 @@ $(function(){
 
 function xout(xbutton){
     popup = xbutton.parentNode.parentNode;
+    console.log(popup);
+    popup.style.display = "none";
+}
+function cancel(xbutton){
+    popup = xbutton.parentNode.parentNode.parentNode;
+    console.log(popup);
     popup.style.display = "none";
 }
 
 
+
 function newPopup(){
     popup = document.getElementById("newFilePopup");
+    popup.style.display = "block";
+}
+
+function preferencesPopup(){
+    popup = document.getElementById("preferencesPopup");
+    popup.style.display = "block";
+}
+
+function adjustmentsPopup(){
+    popup = document.getElementById("adjustmentsPopup");
     popup.style.display = "block";
 }
